c39ce6eb671cbb857372b83caedbcd9100b39856
Vitali Pashek <vipash27@gmail.com>
2020-10-16 15:09:41 +0300

    add solution task 2

--- Main_Task/src/Program.java
+++ Main_Task/src/Program.java
@@ -12,6 +12,9 @@ public class Program {
     public static void main(String[] args) {
         // task 1
         welcomeUser();
+
+        // task 2
+        displayDataInReverseOrder();
     }
 
     // 1. Приветствовать любого пользователя при вводе его имени через командную строку.
@@ -21,6 +24,18 @@ public class Program {
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
         System.out.println("Welcome, " + name + "!");
+    }
+
+    // 2. Отобразить в окне консоли аргументы командной строки в обратном порядке.
+    private static void displayDataInReverseOrder() {
+        System.out.println("--------------- TASK 2 ---------------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        String line = scanner.nextLine();
+        System.out.print("Output: ");
+        for (int i = line.length() - 1; i >= 0; i--) {
+            System.out.print(line.charAt(i));
+        }
     }
 
 }
